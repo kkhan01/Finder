42864795bcfb3524d3f4b061d94aa541bf4bec56
Khinshan Khan <khanshan501@gmail.com>
2019-01-11 14:18:28 -0500

    basic variables for database cols

--- lib/utils/database_helper.dart
+++ lib/utils/database_helper.dart
@@ -3,3 +3,18 @@ import 'dart:async';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_app/models/job.dart';
+
+class DatabaseHelper {
+
+	static DatabaseHelper _databaseHelper;    // Singleton DatabaseHelper
+	static Database _database;                // Singleton Database
+
+	String jobTable = 'job_table';
+	String colId = 'id';
+	String colCompany = 'company';
+	String colLocation = 'location';
+	String colPosition = 'position';
+  String colDescription = 'description';
+  String colUrl = 'url';
+
+}

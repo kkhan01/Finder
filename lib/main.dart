c979d172d58fcbcac75c1e08ef307ae044870bd3
Khinshan Khan <khanshan501@gmail.com>
2019-01-15 16:47:02 -0500

    imports copied over

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+import 'dart:convert';
+import 'dart:async';
+import './models/job.dart';
+

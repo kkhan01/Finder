15961deb1b9d0d18dd05ac6acfc8bd388735416f
Khinshan Khan <khanshan501@gmail.com>
2019-01-20 17:18:46 -0500

    imports

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:glowing_guacamole/models/job_card.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+import 'dart:convert';
+import 'dart:async';
+import './models/job.dart';
+// TODO: merge legacy.dart
+// TODO: merge legacy2.dart
+// TODO: add database_helper.dart
+

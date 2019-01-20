5d04d8b8116a05cd1f8cd97ba1f765d766ff8547
Peter Lee <peterdjlee@gmail.com>
2019-01-20 00:31:20 -0500

    implement text fields for SettingsPage

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         title: Text(
@@ -132,6 +133,7 @@ class MyApp extends StatelessWidget {
 class SavedPage extends MaterialPageRoute<Null> {
   SavedPage() : super(builder: (BuildContext ctx) {
       return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           centerTitle: true,
           title: Text(
@@ -242,7 +244,7 @@ class SavedPage extends MaterialPageRoute<Null> {
                         onPressed: () {
                           // TODO: delete the saved job
                         },
-                      )
+                      ),
                     ],
                   ),
                 ),
@@ -297,8 +299,11 @@ class SavedPage extends MaterialPageRoute<Null> {
 }
 
 class SettingsPage extends MaterialPageRoute<Null> {
+  final positionController = TextEditingController();
+
   SettingsPage() : super(builder: (BuildContext ctx) {
       return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
           centerTitle: true,
           title: Text(
@@ -326,14 +331,46 @@ class SettingsPage extends MaterialPageRoute<Null> {
         // TODO: form for api search
         body: Container(
           color: Colors.white,
-          child: Center(
-            child: MaterialButton(
-              onPressed: (){
-                Navigator.popUntil(ctx, ModalRoute.withName(Navigator.defaultRouteName));
-              },
-              child: Text("Go Home!"),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 16.0,
+              left: 32.0,
+              right: 32.0,
             ),
-          ),
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  // controller: positionController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                        Icons.person,
+                        color: Colors.black,
+                      ),
+                    labelText: "Position",
+                    labelStyle: TextStyle(
+                      color: Colors.black
+                    ),
+                    hintText: "Software Engineer",
+                  ),
+                ),
+                TextField(
+                  // controller: positionController,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                        Icons.location_city,
+                        color: Colors.black,
+                      ),
+                    labelText: "City",
+                    labelStyle: TextStyle(
+                      color: Colors.black
+                    ),
+                    hintText: "New York",
+                  ),
+                ),
+                // TODO: need to implement DropDownButton
+              ],
+            ),
+          )
         ),
       );
   });

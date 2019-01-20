e9f0fa3aeefad1c77a7f97424bca26d36f123952
Peter Lee <peterdjlee@gmail.com>
2019-01-20 00:01:04 -0500

    implement design for SavedPage

--- lib/main.dart
+++ lib/main.dart
@@ -158,12 +158,133 @@ class SavedPage extends MaterialPageRoute<Null> {
         // TODO: umm list out jobs from database, follow task manager as guide
         body: Container(
           color: Colors.white,
-          child: Center(
-            child: RaisedButton(
-              onPressed: (){
-                Navigator.popUntil(ctx, ModalRoute.withName(Navigator.defaultRouteName));
-              },
-              child: Text("Go Home!"),
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Facebook",
+                              style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Software Engineer, Intern/Co-op",
+                              style: TextStyle(
+                                fontSize: 14.0, color: Colors.black
+                              ),
+                            ),
+                          ],
+                        )
+                      ),
+                      SizedBox(width: 6.0),
+                      IconButton(
+                        icon: Icon(
+                          Icons.delete_outline,
+                          size: 36.0,
+                        ),
+                        onPressed: () {
+                          // TODO: delete the saved job
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                  height: 16.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Uber",
+                              style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Product Designer, Uber Eats",
+                              style: TextStyle(
+                                fontSize: 14.0, color: Colors.black
+                              ),
+                            ),
+                          ],
+                        )
+                      ),
+                      SizedBox(width: 6.0),
+                      IconButton(
+                        icon: Icon(
+                          Icons.delete_outline,
+                          size: 36.0,
+                        ),
+                        onPressed: () {
+                          // TODO: delete the saved job
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                Divider(
+                  color: Colors.black,
+                  height: 16.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Amazon",
+                              style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Front End Engineer",
+                              style: TextStyle(
+                                fontSize: 14.0, color: Colors.black
+                              ),
+                            ),
+                          ],
+                        )
+                      ),
+                      SizedBox(width: 6.0),
+                      IconButton(
+                        icon: Icon(
+                          Icons.delete_outline,
+                          size: 36.0,
+                        ),
+                        onPressed: () {
+                          // TODO: delete the saved job
+                        },
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),

8249490844abc121a4dfec6f6a8f1df2bdfdc4dd
Khinshan Khan <khanshan501@gmail.com>
2019-01-20 19:18:04 -0500

    done converting all 3 pages to be stateful. routing is still safe i think

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'dart:convert';
 import 'dart:async';
 import './models/job.dart';
 // TODO: merge legacy.dart
-// TODO: merge legacy2.dart
 // TODO: add database_helper.dart
 
 void main() {
@@ -17,8 +16,8 @@ void main() {
       home: MyApp(),
       routes: <String, WidgetBuilder> {
         '/home': (BuildContext ctx) => new MyApp(),
-        '/saved' : (BuildContext ctx) => new SavedPage()/*,
-        '/settings' : (BuildContext ctx) => new SettingsPage()*/
+        '/saved' : (BuildContext ctx) => new SavedPage(),
+        '/settings' : (BuildContext ctx) => new SettingsPage()
       },
       // theme: themeData,
   ));
@@ -80,7 +79,6 @@ class _MyApp extends State<MyApp> {
             Icons.inbox,
             color: Colors.black,
           ),
-          //onPressed: () => Navigator.push(ctx, SavedPage()),
           onPressed: () => Navigator.of(ctx).pushNamed('/saved'),
         ),
         actions: <Widget>[
@@ -90,7 +88,7 @@ class _MyApp extends State<MyApp> {
               color: Colors.black,
             ),
             onPressed: (){
-              //Navigator.push(ctx, SettingsPage());
+              Navigator.of(ctx).pushNamed('/settings');//Navigator.push(ctx, SettingsPage());
             },
           )
         ],
@@ -293,6 +291,99 @@ class _SavedPageState extends State<SavedPage> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+
+//start
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  int _counter = 0;
+
+  void _savesettings() {
+    setState(() {
+        _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "SETTINGS",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.check,
+              color: Colors.black,
+            ),
+            onPressed: (){
+              // TODO: save settings method called here, are we using another db? we should use a json or smth....
+              Navigator.popUntil(ctx, ModalRoute.withName(Navigator.defaultRouteName));
+            },
+          )
+        ],
+      ),
+      
+      // TODO: form for api search
+      body: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 16.0,
+            left: 32.0,
+            right: 32.0,
+          ),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                // controller: positionController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  labelText: "Position",
+                  labelStyle: TextStyle(
+                    color: Colors.black
+                  ),
+                  hintText: "Software Engineer",
+                ),
+              ),
+              TextField(
+                // controller: positionController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.location_city,
+                    color: Colors.black,
+                  ),
+                  labelText: "City",
+                  labelStyle: TextStyle(
+                    color: Colors.black
+                  ),
+                  hintText: "New York",
+                ),
+              ),
+              // TODO: need to implement DropDownButton
+            ],
+          ),
+        )
       ),
     );
   }
